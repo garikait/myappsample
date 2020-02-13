fbe9b154cc84698f428105d3487dc5568d955414
Garikai Tachiwona <garikai.tachiwona@enterpriseit.co.nz>
2020-02-13 14:35:27 +1300

    test1

---
+++ check_weekv2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo -e "Checking is we should run this script\n====\n"
+if [ -f count_week.txt ]; then week_number=`cat count_week.txt`; else week_number=1; fi
+
+case $week_number in
+
+	echo -e "This is week number  $week_number"
+if [ $week_number -le 3 ]
+   then 
+       week_number=`expr $week_number + 1`
+       echo "New Week Number is $week_number"
+       echo -e "Run /root/scripts/vmbackup_to_risingsun.sh for week_1_to_3 Backup \n====\n"
+       echo  $week_number > count_week.txt  
+    else
+       echo -e "Running /root/scripts/vmbackup_to_risingsun4.sh Week_4 Script \n====\n"
+       echo 1 > count_week.txt 
+fi
+
